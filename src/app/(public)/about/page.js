a3a10b77bb4bd670ff344966109c7c8a6056252b
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-08-23 16:21:11 +0600

    there are a error thrown from about page for testig error.js is working or not

--- src/app/(public)/about/page.js
+++ src/app/(public)/about/page.js
@@ -1,12 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            this is About page
-        
-        </div>
-    );
+  throw new Error("this error from about page loading and made it intentional");
+  return <div>this is About page</div>;
 };
 
 export default About;
