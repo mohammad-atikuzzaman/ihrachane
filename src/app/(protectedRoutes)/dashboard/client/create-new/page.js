9f4b1dde742a8c7e70a1f6a20cfcee748bafd85e
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-08-23 22:00:33 +0600

    make ui design for add new client page

---
+++ src/app/(protectedRoutes)/dashboard/client/create-new/page.js
@@ -0,0 +1,11 @@
+import CreateClientForm from "@/components/dashboard/client/CreateNewClientForm";
+
+const CreateNewClientPage = () => {
+  return (
+    <div className='max-w-2xl mx-auto'>
+      <CreateClientForm />
+    </div>
+  );
+};
+
+export default CreateNewClientPage;
--- src/app/(protectedRoutes)/dashboard/client/page.js
+++ src/app/(protectedRoutes)/dashboard/client/page.js
@@ -9,10 +9,8 @@ const ClientPage = () => {
       <div className='bg-white rounded-lg shadow p-6'>
         <div className='text-end py-3'>
           <button
-            onClick={() =>
-              router.push("/dashboard/categories/client/create-new")
-            }
-            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
+            onClick={() => router.push("/dashboard/client/create-new")}
+            className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'>
             Add Client
           </button>
         </div>
