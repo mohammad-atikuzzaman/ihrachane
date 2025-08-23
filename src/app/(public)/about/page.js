707324ace1e1a45314e6d43b01c3d24aa70f4233
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-08-23 16:47:39 +0600

    error is comment out, if you want to check this just un comment the error

--- src/app/(public)/about/page.js
+++ src/app/(public)/about/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-  throw new Error("this error from about page loading and made it intentional");
+//   throw new Error("this error from about page loading and made it intentional");
   return <div>this is About page</div>;
 };
 
