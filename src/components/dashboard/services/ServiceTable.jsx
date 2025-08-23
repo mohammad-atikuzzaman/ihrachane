5dd567c0b6891b47458093bddd474370a7ad8ade
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-08-23 22:21:10 +0600

    make add service page and service page to show services

---
+++ src/components/dashboard/services/ServiceTable.jsx
@@ -0,0 +1,53 @@
+import { CiEdit } from "react-icons/ci";
+import { AiOutlineDelete } from "react-icons/ai";
+
+const ServiceTable = () => {
+  const data = [
+    { id: 1, title: "client 1" },
+    { id: 2, title: "client 2" },
+    { id: 3, title: "client 3" },
+    { id: 4, title: "client 4" },
+  ];
+
+  return (
+    <div className='overflow-x-auto'>
+      <table className='min-w-full divide-y divide-gray-200'>
+        <thead className='bg-gray-50'>
+          <tr>
+            <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+              SL. NO
+            </th>
+            <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+              TITLE
+            </th>
+            <th className='px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider'>
+              ACTION
+            </th>
+          </tr>
+        </thead>
+        <tbody className='bg-white divide-y divide-gray-200'>
+          {data?.map((item, index) => (
+            <tr key={item.id}>
+              <td className='px-6 py-4 whitespace-nowrap'>
+                <div className='text-sm text-gray-900'>{index + 1}</div>
+              </td>
+              <td className='px-6 py-4 whitespace-nowrap'>
+                <div className='text-sm text-gray-900'>{item.title}</div>
+              </td>
+              <td className='px-6 py-4 whitespace-nowrap flex gap-2 justify-center'>
+                <div className='p-2 rounded-full bg-gray-100 hover:bg-gray-200 w-fit hover:p-2.5'>
+                  <CiEdit className='text-sm hover:text-lg text-gray-900' />
+                </div>
+                <div className='p-2 rounded-full bg-gray-100 hover:bg-gray-200 w-fit hover:p-2.5'>
+                  <AiOutlineDelete className='text-sm hover:text-lg text-gray-900' />
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ServiceTable;
