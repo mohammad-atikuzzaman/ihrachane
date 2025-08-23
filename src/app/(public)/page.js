3a5b05fccebda35aab8ba2cfbdb3a3d0503be26d
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-08-23 16:24:44 +0600

    I have set a loading state for test loadig.js page is woking or not. This loading state set on home page

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -2,8 +2,14 @@ import Clients from "@/components/Home/Clients";
 import Features from "@/components/Home/Features";
 import Services from "@/components/Home/Services";
 import Supplier from "@/components/Home/Supplier";
+import { resolve } from "styled-jsx/css";
 
-export default function Home() {
+export default async function Home() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Loading promise resolved");
+    }, 2000);
+  });
   return (
     <div className="">
       <Features />
