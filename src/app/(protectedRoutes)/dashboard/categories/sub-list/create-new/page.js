35c4158875f38455130c4bb7e47eb079a4a9a76c
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-08-23 15:31:08 +0600

    made ui design for sub categories page list and add method page for this

---
+++ src/app/(protectedRoutes)/dashboard/categories/sub-list/create-new/page.js
@@ -0,0 +1,12 @@
+import CreateSubCategoryForm from '@/components/dashboard/categories/CreateSubCategoryForm';
+import React from 'react';
+
+const CreateNewSubListPage = () => {
+    return (
+        <div>
+            <CreateSubCategoryForm/>
+        </div>
+    );
+};
+
+export default CreateNewSubListPage;
