467c02d69cf45830a04226091e56591df8217db8
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-08-25 23:36:30 +0600

    added all

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -2,6 +2,7 @@ import Clients from "@/components/Home/Clients";
 import Features from "@/components/Home/Features";
 import Services from "@/components/Home/Services";
 import Supplier from "@/components/Home/Supplier";
+import Form from "@/components/shared/Form";
 
 export default async function Home() {
   await new Promise((resolve) => {
@@ -11,10 +12,12 @@ export default async function Home() {
   });
   return (
     <div className="">
+    
       <Features />
       <Services />
       <Supplier />
       <Clients />
+      <Form/>
     </div>
   );
 }
