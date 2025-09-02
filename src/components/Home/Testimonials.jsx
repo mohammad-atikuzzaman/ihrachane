5fefb805519f7543445cc91878efbe66d5fda3b5
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-09-03 00:33:46 +0600

    fex

--- src/components/Home/Testimonials.jsx
+++ src/components/Home/Testimonials.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { getData } from "@/utils/axiosPublic";
 
 const Testimonials = () => {
-  
+
   const [testimonials, setTestimonials] = useState([]);
   useEffect(() => {
     async function fetchData() {
